b3d8403b8673d930305eed4e15db6a09bec3edfe
Kushan Umayanga <kushanbandara318@gmail.com>
2026-06-15 00:01:59 +0530

    Completed shared model classes

--- src/greenloop/model/Notification.java
+++ src/greenloop/model/Notification.java
@@ -1,4 +1,20 @@
 package greenloop.model;
 
 public class Notification {
+    private String notificationId;
+    private String receiverEmail;
+    private String message;
+    private String type;
+
+    public Notification(String notificationId, String receiverEmail, String message, String type) {
+        this.notificationId = notificationId;
+        this.receiverEmail = receiverEmail;
+        this.message = message;
+        this.type = type;
+    }
+
+    public String getNotificationId() { return notificationId; }
+    public String getReceiverEmail() { return receiverEmail; }
+    public String getMessage() { return message; }
+    public String getType() { return type; }
 }
